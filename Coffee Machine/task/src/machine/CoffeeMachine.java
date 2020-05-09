48982b07d91a21184b55d716b7ec424aff68d2ba
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2020-05-10 00:48:34 +0300

    Init

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -5,59 +5,12 @@ import java.util.Scanner;
 public class CoffeeMachine {
     public static void main(String[] args) {
         setInitialIngredients();
-        getCurrentState();
         processAction();
-        getCurrentState();
     }
-    static final int WATER = 200;
-    static final int MILK = 50;
-    static final int COFFEE_BEANS = 15;
 
     static Scanner scanner = new Scanner(System.in);
     static int currentWater, currentMilk, currentCoffeeBeans, currentDisposableCups, currentMoney;
-
-    static void calculateIngredients() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Write how many cups of coffee you will need: ");
-        int count = Integer.parseInt(scanner.nextLine());
-
-        System.out.printf("For %d cups of coffee you will need: %n %d ml of water %n %d ml of milk %n %d g of coffee beans",
-                count, count * WATER, count * MILK, count * COFFEE_BEANS);
-    }
-
-    static void calculateCupsOfCoffee() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Write how many cups of coffee you will need: ");
-        int count = Integer.parseInt(scanner.nextLine());
-
-        int possibleCupsOfCoffee = getPossibleCupsOfCoffee();
-        if (possibleCupsOfCoffee == count) {
-            System.out.println("Yes, I can make that amount of coffee");
-        } else if (possibleCupsOfCoffee > count) {
-            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)", possibleCupsOfCoffee - count);
-        } else {
-            System.out.printf("No, I can make only %d cup(s) of coffee", possibleCupsOfCoffee);
-        }
-    }
-
-    static int getPossibleCupsOfCoffee() {
-        return Math.min(Math.min(currentWater / WATER, currentMilk / MILK), currentCoffeeBeans / COFFEE_BEANS);
-    }
-
-    static void getCurrentIngredients() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Write how many ml of water the coffee machine has: ");
-        currentWater = Integer.parseInt(scanner.nextLine());
-
-        System.out.println("Write how many ml of milk the coffee machine has: ");
-        currentMilk = Integer.parseInt(scanner.nextLine());
-
-        System.out.println("Write how many grams of coffee beans the coffee machine has: ");
-        currentCoffeeBeans = Integer.parseInt(scanner.nextLine());
-    }
+    static boolean machineWorked;
 
     static void setInitialIngredients() {
         currentWater = 400;
@@ -65,53 +18,74 @@ public class CoffeeMachine {
         currentCoffeeBeans = 120;
         currentDisposableCups = 9;
         currentMoney = 550;
-    }
-
-    static void getCurrentState() {
-        System.out.println("The coffee machine has:");
-        System.out.printf("%d of water%n", currentWater);
-        System.out.printf("%d of milk%n", currentMilk);
-        System.out.printf("%d coffee beans%n", currentCoffeeBeans);
-        System.out.printf("%d of disposable cups%n", currentDisposableCups);
-        System.out.printf("%d of money%n", currentMoney);
+        machineWorked = true;
     }
 
     static void processAction() {
-        System.out.println("Write action (buy, fill, take):");
-        switch (scanner.nextLine()) {
-            case "buy":
-                buy();
-                break;
-            case "fill":
-                fill();
-                break;
-            case "take":
-                take();
-                break;
+        while (machineWorked) {
+            System.out.println("Write action (buy, fill, take, remaining, exit):");
+            switch (scanner.nextLine()) {
+                case "buy":
+                    buy();
+                    break;
+                case "fill":
+                    fill();
+                    break;
+                case "take":
+                    take();
+                    break;
+                case "remaining":
+                    remaining();
+                    break;
+                case "exit":
+                    exit();
+                    break;
+                default:
+                    System.out.println("Unknown action");
+            }
         }
     }
 
     static void buy() {
-        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
-        switch (Integer.parseInt(scanner.nextLine())) {
-            case 1:
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
+        switch (scanner.nextLine()) {
+            case "1":
                 makeCoffee(250, 0, 16, 4);
                 break;
-            case 2:
+            case "2":
                 makeCoffee(350, 75, 20, 7);
                 break;
-            case 3:
+            case "3":
                 makeCoffee(200, 100, 12, 6);
                 break;
+            case "back":
+                processAction();
+                break;
+            default:
+                System.out.println("Unknown command");
         }
     }
 
     static void makeCoffee(int water, int milk, int coffeeBeans, int money) {
-        currentWater -= water;
-        currentMilk -= milk;
-        currentCoffeeBeans -= coffeeBeans;
-        currentMoney += money;
-        currentDisposableCups--;
+
+        if (currentWater < water) {
+            System.out.println("Sorry, not enough water!");
+        } else if (currentMilk < milk) {
+            System.out.println("Sorry, not enough milk!");
+        } else if (currentCoffeeBeans < coffeeBeans) {
+            System.out.println("Sorry, not enough coffee beans!");
+        } else if (currentDisposableCups < 1) {
+            System.out.println("Sorry, not enough disposable cups!");
+        } else {
+
+            System.out.println("I have enough resources, making you a coffee!");
+
+            currentWater -= water;
+            currentMilk -= milk;
+            currentCoffeeBeans -= coffeeBeans;
+            currentMoney += money;
+            currentDisposableCups--;
+        }
     }
 
     static void fill() {
@@ -131,5 +105,18 @@ public class CoffeeMachine {
     static void take() {
         currentMoney = 0;
         System.out.printf("I gave you $%d%n", currentMoney);
+    }
+
+    static void remaining() {
+        System.out.println("The coffee machine has:");
+        System.out.printf("%d of water%n", currentWater);
+        System.out.printf("%d of milk%n", currentMilk);
+        System.out.printf("%d coffee beans%n", currentCoffeeBeans);
+        System.out.printf("%d of disposable cups%n", currentDisposableCups);
+        System.out.printf("%d of money%n", currentMoney);
+    }
+
+    static void exit() {
+        machineWorked = false;
     }
 }
